966b3873804d669fd8b705f0822a59b2798987a8
Praval Singh Chawla <chawlapravals@gmail.com>
2026-01-05 18:22:01 +0100

    Implemented Ridge regression! It is now possible to import Ridge and Logistic!

--- pravalml/__init__.py
+++ pravalml/__init__.py
@@ -1,3 +1,5 @@
 from .models.linear_regression import LinearRegression
+from .models.logistic_regression import LogisticRegression
+from .models.ridge_regression import RidgeRegression
 
-__all__ = ["LinearRegression"]
+__all__ = ["LinearRegression", "LogisticRegression", "RidgeRegression"]
