fab1eb4c9810b96cd68b3b9fa7479b797066adf2
Praval Singh Chawla <chawlapravals@gmail.com>
2026-01-05 19:06:32 +0100

    Made it possible to import lasso

--- pravalml/__init__.py
+++ pravalml/__init__.py
@@ -1,5 +1,6 @@
 from .models.linear_regression import LinearRegression
 from .models.logistic_regression import LogisticRegression
 from .models.ridge_regression import RidgeRegression
+from .models.lasso_regression import LassoRegression
 
-__all__ = ["LinearRegression", "LogisticRegression", "RidgeRegression"]
+__all__ = ["LinearRegression", "LogisticRegression", "RidgeRegression", "LassoRegression"]
