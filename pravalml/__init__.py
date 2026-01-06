762ff7b3fefec7377943d1d0c599f1109ae9a59d
Praval Singh Chawla <chawlapravals@gmail.com>
2026-01-06 17:56:30 +0100

    MLPRegressos and MLPClassifier are now avaible for imports

--- pravalml/__init__.py
+++ pravalml/__init__.py
@@ -2,5 +2,6 @@ from .models.linear_regression import LinearRegression
 from .models.logistic_regression import LogisticRegression
 from .models.ridge_regression import RidgeRegression
 from .models.lasso_regression import LassoRegression
+from .models.mlp import MLPClassifier, MLPRegressor
 
-__all__ = ["LinearRegression", "LogisticRegression", "RidgeRegression", "LassoRegression"]
+__all__ = ["LinearRegression", "LogisticRegression", "RidgeRegression", "LassoRegression", "MLPClassifier", "MLPRegressor"]
